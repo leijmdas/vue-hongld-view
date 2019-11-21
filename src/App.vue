81d84ea275bcaa5137a0f520563e72e0f7a9f1ea
leijmdas@163.com <leijmdas@163.com>
2019-11-21 17:46:34 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
 <template>
   <div id="app">
     <!--<router-view />-->
-  <div>
-    <img src="@/assets/images/hld.jpg" align="middle"/>
-  </div>
-    <div>
-    <img src="@/assets/images/girl.jpg"/>
+    <div align="middle"><img src="@/assets/images/welcome.jpg"/></div>
+    <div align="middle">
+      <img src="@/assets/images/hld.jpg"/>
     </div>
+    <div align="middle">
+      <img src="@/assets/images/banner1.jpg"/>
+    </div>
+    <div style="position:relative;">
+      <img src="@/assets/images/bottom.jpg" width="100%"/>
+      　　
+      <div    style="position:relative; z-index:2;   top:-100px; text-align:center; ">深圳市宏利达海绵有限公司 版权所有 2019-2020 All CopyRights
+        Reserve.
+      </div>
 
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  export default {
+    name: 'App'
+  }
 </script>
