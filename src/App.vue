ff1c62126fb5a54e6dcb022e91fe96f0fc81659d
leijmdas@163.com <leijmdas@163.com>
2019-11-25 14:08:02 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -97,7 +97,7 @@
 
     <div style="position:relative;">
       <img src="@/assets/images/bottom.jpg" width="100%"/>
-      　　
+      　　<div style="color:#fef0f0 ;position:relative; z-index:2;   top:-120px; text-align:center; ">地址： 中国 广东 深圳 龙岗区布吉大芬村和通花园3栋405（办公场所） </div>
       <div style="position:relative; z-index:2;   top:-100px; text-align:center; ">深圳市宏利达海绵有限公司 版权所有 2019-2020 All
         CopyRights
         Reserve.
