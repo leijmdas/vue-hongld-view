9ddf9f53ab39faf1dba66ad2063f24e130435b82
leijmdas@163.com <leijmdas@163.com>
2019-11-29 15:50:40 +0800

    add

--- src/views/left/index.vue
+++ src/views/left/index.vue
@@ -13,40 +13,39 @@
       <div class="div-c">凝胶梦幻记忆</div>
     </div>
 
-    <div class="div-a" >
+    <div class="div-a">
       <img src="@/assets/images/company/prd/contact.jpg"/>
+      <br/>
+      <br/>
+
       <el-row>
-        <el-col>
-          <el-row>
-            <div class="div-a">
-              联系人：朱金星先生
-            </div>
-          </el-row>
-          <el-row>
-            <div class="div-a">
-              电 话：86 755 29736896-807
-            </div>
-          </el-row>
-          <el-row>
-            <div class="div-a">电 话：13538116703</div>
-          </el-row>
-          <el-row>
-            <div class="div-a">传 真：86 755 29736003</div>
-          </el-row>
-
-          <el-row>
-            <div class="div-a">邮 箱：xxx@163.com</div>
-          </el-row>
-
-          <el-row>
-            <div class="div-b">地 址：中国 广东 深圳市龙岗区 平湖镇辅城坳凤岐路2-2号</div>
-          </el-row>
-          <el-row>
-            <div class="div-a">邮 编：518000</div>
-          </el-row>
-
-        </el-col>
+        <div class="div-a">
+          联系人：朱金星先生
+        </div>
       </el-row>
+      <el-row>
+        <div class="div-a">
+          电&nbsp;&nbsp;话：86 755 29736896-807
+        </div>
+      </el-row>
+      <el-row>
+        <div class="div-a">电 话：13538116703</div>
+      </el-row>
+      <el-row>
+        <div class="div-a">传 真：86 755 29736003</div>
+      </el-row>
+
+      <el-row>
+        <div class="div-a">邮 箱：xxx@163.com</div>
+      </el-row>
+
+      <el-row>
+        <div class="div-b">地 址：中国 广东 深圳市龙岗区 平湖镇辅城坳凤岐路2-2号</div>
+      </el-row>
+      <el-row>
+        <div class="div-a">邮 编：518000</div>
+      </el-row>
+
     </div>
   </div>
 </template>
@@ -57,13 +56,7 @@
       return {
         form: {
           name: '',
-          region: '',
-          date1: '',
-          date2: '',
-          delivery: false,
-          type: [],
-          resource: '',
-          desc: ''
+
         }
       }
     },
@@ -99,8 +92,9 @@
     height: 50px;
     style: "background:#000; color:#FFF"
   }
+
   .div-c {
-    margin-left:10px;
+    margin-left: 10px;
     float: center;
     height: 20px;
     style: "background:#000; color:#FFF"
